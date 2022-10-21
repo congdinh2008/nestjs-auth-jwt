9a9b7e722bf055c4972e9b5743dc19b004d50696
Cong Dinh <congdinhvannguyen165@outlook.com>
2022-10-21 15:44:19 +0700

    Custom PORT in environment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 declare const module: any;
 
@@ -16,7 +17,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(app.get(ConfigService).get('PORT'));
 
   if (module.hot) {
     module.hot.accept();
